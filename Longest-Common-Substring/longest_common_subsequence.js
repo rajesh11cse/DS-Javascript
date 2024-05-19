0db066e5c1af4a723d41231001d75325efb625e4
RAJESH KUMAR <rajeshkumar11cse@gmail.com>
2024-05-19 14:13:14 +0530

    update

--- Longest-Common-Substring/longest_common_subsequence.js
+++ Longest-Common-Substring/longest_common_subsequence.js
@@ -1,59 +1,71 @@
-// Longest Common Subsequence
-// Input : X = "aggtab", y = "gxtxayb"
-// Output : 4
-// The longest common subsequence is "gtab" and is of length 4.
+// Dynamic Programming
 
-class longestCommonString {
-    // Dynamic Programming METHOD ==> O(nxm)
-    lcsDP(X, Y, m, n) {
-        // Create a table to store lengths of longest common substring of substrings;
-        let L = Array(m + 1).fill(0).map(() => Array(n + 1).fill(0));
+// Approach - Longest Common Subsequence (LCS) using Bottom-Up (Tabulation)
 
-        /* Following steps build L[m+1][n+1] in bottom up fashion. Note 
-             that L[i][j] contains length of LCS of X[0..i-1] and Y[0..j-1] */
-        for (let i = 1; i <= m; i++) {
-            for (let j = 1; j <= n; j++) {
-                if (X.charAt(i - 1) == Y.charAt(j - 1))
-                    L[i][j] = L[i - 1][j - 1] + 1;
-                else
-                    L[i][j] = Math.max(L[i - 1][j], L[i][j - 1]);
-            }
-        }
-        console.log("Length of LCS is", L[m][n])
-        this.printLCSub(X, Y, L, m, n);
-        // return L[m][n];
+function lcs(X, Y, m, n) {
+    var L = new Array(m + 1);
+    for (var i = 0; i < L.length; i++) {
+      L[i] = new Array(n + 1);
     }
-
-    // Print longest common subsequence
-    printLCSub(X, Y, lcs2D, m, n) {
-        let result = [];
-        while (m >= 0 && n >= 0) {
-            // IF the character in both X and Y at position m and n are same then
-            // current character is part of LCS 
-            if (X.charAt(m - 1) == Y.charAt(n - 1)) {
-                result.unshift(X.charAt(m - 1))
-                m--;
-                n--;
-            } else {
-                // If not same, then find the larger of two and 
-                // go in the direction of larger value 
-                lcs2D[m - 1][n] > lcs2D[m][n - 1] ? m-- : n--;
-            }
-
+    var i, j;
+  
+    for (i = 0; i <= m; i++) {
+      for (j = 0; j <= n; j++) {
+        if (i == 0 || j == 0) {
+          L[i][j] = 0;
+        } else if (X[i - 1] == Y[j - 1]) {
+          L[i][j] = L[i - 1][j - 1] + 1;
+        } else {
+          L[i][j] = Math.max(L[i - 1][j], L[i][j - 1]);
         }
-        console.log(result.join('') ? result.join('') : 'Not found')
+      }
     }
-}
+    return L[m][n];
+    // printLCS(X, Y, L, m, n);
+  }
+  
+  // Print longest common subsequence
+  function printLCS(X, Y, lcs2D, m, n) {
+    let result = [];
+    while (m >= 0 && n >= 0) {
+      // IF the character in both X and Y at position m and n are same then
+      // current character is part of LCS
+      if (X.charAt(m - 1) == Y.charAt(n - 1)) {
+        result.unshift(X.charAt(m - 1));
+        m--;
+        n--;
+      } else {
+        // If not same, then find the larger of two and
+        // go in the direction of larger value
+        lcs2D[m - 1][n] > lcs2D[m][n - 1] ? m-- : n--;
+      }
+    }
+    console.log(result.join("") ? result.join("") : "Not found");
+  }
+  
+  // Driver code
+  var S1 = "AGGTAB";
+  var S2 = "GXTXAYB";
+  
+  var m = S1.length;
+  var n = S2.length;
+  
+  console.log("Length of LCS is " + lcs(S1, S2, m, n)); // 4
+  
+  /* 
+      s1 = A[G]G[T][A][B]
+      s2 = [G]X[T]X[A]Y[B]
 
-/* let X = "aggtab";
-let Y = "gxtxayb"; */
+      Output = [G][T][A][B]
+   */
+  
+  /* Follow the dynamic tables steps from here. */
+  // https://www.geeksforgeeks.org/longest-common-subsequence-dp-4/
 
-let X = "ABCDGH";
-let Y = "AEDFHR";
+  // Time Complaxity - O(m * n)
+  // Auxiliary Space: O(m * n)
 
-let m = X.length;
-let n = Y.length;
-var lcs = new longestCommonString();
-
-lcs.lcsDP(X, Y, m, n, 0)
-// console.log(lcs.lcsDP(X, Y, m, n, 0));
+  /* Space-Optimization */
+  // Longest Common Subsequence (LCS) using Bottom-Up (Space-Optimization):
+  // Auxiliary Space: O(m)
+  
--- Longest-Common-Substring/longest_common_substring.js
+++ Longest-Common-Substring/longest_common_substring.js
@@ -4,42 +4,43 @@
 // The longest common substring is “Geeks” and is of length 5.
 
 class longestCommonString {
-
     // Dynamic Programming METHOD ==> O(nxm)
     lcsDP(X, Y, m, n) {
-        // Create a table to store lengths of longest common suffixes of substrings;
-        let LCSuff = Array(m+1).fill(0).map(() => Array(n+1).fill(0));
-        let result = 0;
-        
-        for (let i = 1; i <= m; i++) {
-            for (let j = 1; j <= n; j++) {
-                if(X.charAt(i-1) == Y.charAt(j-1)){
-                    LCSuff[i][j] = LCSuff[i-1][j-1] + 1;
-                    result = Math.max(result, LCSuff[i][j])
-                }
+    // Create a table to store lengths of longest common suffixes of substrings;
+    let LCSuff = Array(m+1).fill(0).map(() => Array(n+1).fill(0));
+    let result = 0;
+    
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if(X.charAt(i-1) == Y.charAt(j-1)){
+                LCSuff[i][j] = LCSuff[i-1][j-1] + 1;
+                result = Math.max(result, LCSuff[i][j])
             }
         }
-        return result;
     }
+    return result;
+}
 
 
-    // Recursion Method  ==> O(n+m)
-    // Returns length of function for longest common  
-    // substring of X[0..m-1] and Y[0..n-1]  
-    lcsRec(X, Y, i, j, count) {
-        // base case
-        if(i == 0 || j == 0){
-            return count;
-        }
-        if(X.charAt(i-1) == Y.charAt(j-1)){
-            count = this.lcsRec(X, Y, i-1, j-1, count+1);
-        }
-        count = Math.max(count, (Math.max(
-            this.lcsRec(X, Y, i, j-1, 0),
-            this.lcsRec(X, Y, i-1, j, 0)
-        )))
+// Recursion Method  ==> O(n+m)
+// Returns length of function for longest common  
+// substring of X[0..m-1] and Y[0..n-1]  
+lcsRec(X, Y, i, j, count) {
+    // base case
+    if(i == 0 || j == 0){
         return count;
     }
+    if(X.charAt(i-1) == Y.charAt(j-1)){
+        count = this.lcsRec(X, Y, i-1, j-1, count+1);
+    }
+    count = Math.max(count, (Math.max(
+        this.lcsRec(X, Y, i, j-1, 0),
+        this.lcsRec(X, Y, i-1, j, 0)
+    )))
+    return count;
+}
+// Time complexity: O(2^max(m,n))
+// Auxiliary Space: O(1)
 
 }
 
@@ -48,6 +49,14 @@ let Y = "xyzabcd";
 let m = X.length;
 let n = Y.length;
 var lcs = new longestCommonString();
+
+
+/* 
+  s1 = [abcd]xyz
+  s2 = xyz[abcd]
+  
+  Output = abcd
+*/
 
 console.log(lcs.lcsDP(X, Y, m, n, 0)); 
 console.log(lcs.lcsRec(X, Y, m, n, 0)); 
